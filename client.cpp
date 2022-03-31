88ad2d3ebe9069be9fea59a36e797d6091b17b9b
tenquitran <tenquitran@gmail.com>
2022-03-31 21:23:12 +0300

    Created server and client skeletons (without any command-line options)

--- client.cpp
+++ client.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <string>
 #include <boost/asio.hpp>
+
+using namespace boost;
+
 
 int main()
 {
-    boost::asio::io_service io;
-    
-    ;
+	// TODO: hard-coded
+	const std::string ipStr = "127.0.0.1";
+    const unsigned short port = 8976;
+
+	try
+	{
+		asio::ip::tcp protocol = asio::ip::tcp::v4();
+		
+		asio::ip::tcp::endpoint ep(asio::ip::address::from_string(ipStr), port);
+		
+		asio::io_service io;
+		
+		asio::ip::tcp::socket sock(io, ep.protocol());
+		
+		sock.connect(ep);
+		
+		// TODO: interact with the server
+    }
+	catch (system::system_error& ex)
+	{
+		std::cerr << "Exception: " << ex.what() << '\n';
+		//std::cerr << "Exception: " << ex.what() << " (" << ex.code() << ")\n";
+		return 1;
+	}
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+using namespace boost;
+
+
 int main()
 {
-    boost::asio::io_service io;
-    
-    ;
+	// TODO: hard-coded
+    const unsigned short port = 8976;
 
+	try
+	{
+		asio::ip::tcp protocol = asio::ip::tcp::v4();
+		
+		asio::ip::address serverIp = asio::ip::address_v4::any();
+
+		asio::ip::tcp::endpoint ep = asio::ip::tcp::endpoint(serverIp, port);
+		
+		asio::io_service io;
+		
+		asio::ip::tcp::acceptor acceptor = asio::ip::tcp::acceptor(io, ep);
+
+		acceptor.listen();
+		
+		// TODO: implement more graceful exit
+		std::cout << "Press Ctrl+C to stop the server" << std::endl;
+		
+		while (true)
+		{
+			asio::ip::tcp::socket sock(io);
+			
+			acceptor.accept(sock);
+			
+			std::string clientIp = sock.remote_endpoint().address().to_string();
+			
+			unsigned short clientPort = sock.remote_endpoint().port();
+			
+			std::cout << "Accepted client connection: " << clientIp << ":" << clientPort << std::endl;
+			
+			// TODO: serve the client
+			;
+		}
+	}
+	catch (system::system_error& ex)
+	{
+		std::cerr << "Exception: " << ex.what() << '\n';
+		//std::cerr << "Exception: " << ex.what() << " (" << ex.code() << ")\n";
+		return 1;
+	}
+	
     return 0;
 }
 
